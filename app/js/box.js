5f04892fe310ccfd7c0ab8e029c316c51b6c5a72
ATHENAxD <jeffluk64@yahoo.com.hk>
2016-11-13 10:31:15 +0800

    Add files via upload

---
+++ app/js/box.js
@@ -0,0 +1,23 @@
+var box = document.getElementById('myBox');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    box.style.display = "block";
+}
+span.onclick = function() {
+   box.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == box) {
+        box.style.display = "none";
+    }
+}
